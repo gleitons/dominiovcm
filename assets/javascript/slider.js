8bddd5ba1e2ed845b76ee77f73a1e48b1aef1a97
hd <hd@iMac-de-hd.local>
2022-06-07 07:27:26 -0300

    form

--- assets/javascript/slider.js
+++ assets/javascript/slider.js
@@ -2,3 +2,17 @@ function mechame() {
     alert("PROCURE POR ALEX BRASIL");
     location.href = "https://goo.gl/maps/n5TCHiNU2Hb5weK86";
 }
+
+function formEnv() {
+    const nome = document.getElementById('nome').value;
+    const telefone = document.getElementById('tel').value;
+    const email = document.getElementById('email').value;
+
+    const infoW = `Olá Alex Brasil, meu nome é ${nome} meu telefone é ${telefone} e meu e-mail é ${email}, gostaria de mais informações do projeto que eu tenho`
+
+    const nomeS = JSON.stringify(infoW);
+    console.log(nomeS)
+
+    const dados = localStorage.setItem('whats',`${nomeS}`);
+    href = ""
+}
